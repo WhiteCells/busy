c881cf7b25de874c1ea9d82745c2bc48f5e74af3
WhiteCells <xuan2692659908@foxmail.com>
2026-03-12 20:04:57 +0800

    update entrypoint.sh

--- app/docker-entrypoint.sh
+++ app/docker-entrypoint.sh
@@ -1,47 +1,94 @@
 #!/bin/bash
 set -e
 
-# speedtest
-if [ $SPEEDTEST_INTERVAL ]; then
-    # create crontab
-    if [ $SPEEDTEST_INTERVAL -eq 0 ]; then
-        echo "Interval is 0, no task will be created"
-    elif [ $SPEEDTEST_INTERVAL -le 59 ]; then
-        echo -e "*/${SPEEDTEST_INTERVAL}\t*\t*\t*\t*\tnohup bash /app/speedtest.sh >/app/speedtest.log 2>&1 &" | crontab
-        echo "Scheduled task created successfully"
-        service cron restart
-    elif [[ $SPEEDTEST_INTERVAL -gt 59 && $SPEEDTEST_INTERVAL -lt 1440 ]]; then
-        hour=$(($SPEEDTEST_INTERVAL / 60))
-        minute=$(($SPEEDTEST_INTERVAL % 60))
-        echo -e "$minute\t*/$hour\t*\t*\t*\tnohup bash /app/speedtest.sh >/app/speedtest.log 2>&1 &" | crontab
-        echo "Scheduled task created successfully"
-        service cron restart
+########################################
+# Configurable variables
+########################################
+
+LOG_FILE="${LOG_FILE:-/app/st.log}"
+SCRIPT_PATH="${SCRIPT_PATH:-/app/st.sh}"
+
+NETTEST_TIMER="${NETTEST_TIMER:-0}"   # minutes
+CPU_UTIL="${CPU_UTIL:-50}"            # CPU usage %
+MEM_UTIL="${MEM_UTIL:-50}"            # memory usage %
+
+########################################
+# Log function
+########################################
+
+log(){
+    echo "[$(date '+%Y-%m-%d %H:%M:%S')] $1" >> "$LOG_FILE"
+}
+
+########################################
+# Create cron job
+########################################
+
+if [ -n "$NETTEST_TIMER" ]; then
+
+    if [ "$NETTEST_TIMER" -eq 0 ]; then
+        log "Interval is 0, no task will be created"
+
+    elif [ "$NETTEST_TIMER" -le 59 ]; then
+
+        CRON="*/${NETTEST_TIMER} * * * * bash $SCRIPT_PATH >> $LOG_FILE 2>&1"
+
+        (crontab -l 2>/dev/null | grep -v "$SCRIPT_PATH" ; echo "$CRON") | crontab -
+
+        log "Cron job created: every ${NETTEST_TIMER} minutes"
+
+    elif [ "$NETTEST_TIMER" -lt 1440 ]; then
+
+        hour=$((NETTEST_TIMER / 60))
+        minute=$((NETTEST_TIMER % 60))
+
+        CRON="$minute */$hour * * * bash $SCRIPT_PATH >> $LOG_FILE 2>&1"
+
+        (crontab -l 2>/dev/null | grep -v "$SCRIPT_PATH" ; echo "$CRON") | crontab -
+
+        log "Cron job created: every ${NETTEST_TIMER} minutes"
+
     else
-        echo "Interval limit exceeded"
+        log "Interval limit exceeded"
     fi
 
+    service cron restart
+
 else
-    echo "SPEEDTEST_INTERVAL is not exists"
+    log "NETTEST_TIMER is not set"
 fi
 
-# lookbusy
-MemTotal=$(awk '($1 == "MemTotal:"){printf "%d\n",$2/1024}' /proc/meminfo) # total memory
-MemUsage=$(($MemTotal / 100 * $MEM_UTIL))
-# lb -c $CPU_UTIL -n $CPU_CORE -m ${MemUsage}MB -r curve
-lb -c $CPU_UTIL -m ${MemUsage}MB -r curve
 
-# if [ $CPU_CORE ]; then
-#     if [ $MEM_UTIL ]; then
-#         MemUsage=$(($MemTotal / 100 * $MEM_UTIL))
-#         lookbusy -c $CPU_UTIL -n $CPU_CORE -m ${MemUsage}MB
-#     else
-#         lookbusy -c $CPU_UTIL -n $CPU_CORE
-#     fi
-# else
-#     if [ $MEM_UTIL ]; then
-#         MemUsage=$(($MemTotal / 100 * $MEM_UTIL))
-#         lookbusy -c $CPU_UTIL -r curve -m ${MemUsage}MB
-#     else
-#         lookbusy -c $CPU_UTIL -r curve
-#     fi
-# fi
+########################################
+# Speedtest
+########################################
+
+run_speedtest(){
+
+    log "Speedtest started"
+
+    RESULT=$(speedtest --single 2>&1)
+
+    log "$RESULT"
+
+    log "Speedtest finished"
+}
+
+run_speedtest
+
+
+########################################
+# Memory calculation
+########################################
+
+MemTotal=$(awk '/MemTotal/ {printf "%d\n",$2/1024}' /proc/meminfo)
+
+MemUsage=$((MemTotal * MEM_UTIL / 100))
+
+log "Starting CPU load: $CPU_UTIL%  Memory: ${MemUsage}MB"
+
+########################################
+# Start CPU / memory load
+########################################
+
+lb -c "$CPU_UTIL" -m "${MemUsage}MB" -r curve
